67297ef5517a38026861656961af8b08bc4228d1
xiewenli <1223260658@qq.com>
2022-12-23 23:07:33 +0800

    完成echarts和初步展示user页面

--- src/views/main/echarts/echarts.vue
+++ src/views/main/echarts/echarts.vue
@@ -21,18 +21,22 @@ Promise.all([rq1, rq2]).then(res => {
   option.series[0].data[1] = res[1].data.message.memory
   var myChart = echarts.init(divEl.value);
   myChart.setOption(option);
+}).catch(err => {
+  console.log(err);
 })
 
 </script>
 
 <style lang="less" scoped>
 .echarts {
+  background-color: white;
   width: 100%;
   height: 100%;
+  padding: 10px 0;
 }
 
 .divEl {
-  background-color: white;
+
   height: 600px;
 }
 </style>
